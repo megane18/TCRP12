814861f47da0563d0e8842f5f820000748bf3160
Logan Bolton <loganbolton101@gmail.com>
2024-09-21 01:40:54 -0400

    add sign up page

--- tcrp/src/App.jsx
+++ tcrp/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import EventManager from './components/EventManager';
 import './App.css';
+import SignUpForm from './components/SignUpForm';
 
 
 const App = () => {
   return (
     <div className ="content">
-      <EventManager />
+      {/* <EventManager /> */}
+      <SignUpForm />
     </div>
   );
 }
