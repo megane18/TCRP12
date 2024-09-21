fa360998429123982437918f2705e001d68a4018
Oluwatodimu Adegoke <adegokeoluwatodimu@yahoo.com>
2024-09-21 13:33:53 -0500

    footer

--- tcrp/src/components/SendMassCommunication.jsx
+++ tcrp/src/components/SendMassCommunication.jsx
@@ -7,13 +7,16 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 const SendMassCommunication = () => {
-  const [convertedText, setConvertedText] = useState("Message");
+  const [convertedText, setConvertedText] = useState(
+    "Compose your message here"
+  );
   const options = ["All Events", "Volunteer Day", "H.A.N.D.S"];
   const defaultOption = options[0];
+
   return (
-    <div className="bg-white flex flex-col h-screen w-screen">
+    <div className=" bg-white flex flex-col h-screen w-screen">
       <Header />
-      <div className="bg-[#B7C9D3] mt-6 mx-2 overflow-hidden rounded-xl p-2 flex flex-col">
+      <div className="bg-[#B7C9D3] mt-6 mx-2 flex-grow rounded-xl p-4 flex flex-col overflow-clip">
         <div className="text-black flex flex-col text-start">
           <h2 className="font-bold text-xl">Send Mass Communication</h2>
           <h4>Reach out to your community members efficiently</h4>
@@ -31,12 +34,12 @@ const SendMassCommunication = () => {
             <p className="text-start text-gray-500">Message</p>
             <ReactQuill
               theme="snow"
-              className="min-h-100 bg-white rounded-md text-black"
+              className=" bg-white rounded-md text-black"
               value={convertedText}
               onChange={setConvertedText}
             />
           </div>
-          <div className="justify-start flex">
+          <div className="flex justify-start">
             <button className="bg-black text-white rounded-md h-10 p-2">
               Attach File
             </button>
@@ -69,37 +72,35 @@ const SendMassCommunication = () => {
               </div>
               <div>
                 <h3 className="text-black text-start">Events</h3>
-                <div>
-                  <Dropdown
-                    options={options}
-                    value={defaultOption}
-                    placeholder="Select an Event"
-                    className="w-30"
-                  />
-                </div>
+                <Dropdown
+                  options={options}
+                  value={defaultOption}
+                  placeholder="Select an Event"
+                  className="w-30"
+                />
               </div>
             </div>
           </div>
           <div>
-            <p className="text-start text-gray-500">Channel selection</p>
+            <p className="text-start text-gray-500">Channel Selection</p>
             <div className="bg-white w-full rounded-md flex p-2 flex-row justify-between">
               <div className="flex gap-4">
                 <div className="flex items-center">
-                  <input type="radio" name="channel" id="allMembers" />
-                  <label htmlFor="allMembers" className="text-black ml-2">
+                  <input type="radio" name="channel" id="emailChannel" />
+                  <label htmlFor="emailChannel" className="text-black ml-2">
                     Email
                   </label>
                 </div>
                 <div className="flex items-center">
-                  <input type="radio" name="channel" id="newMembers" />
-                  <label htmlFor="newMembers" className="text-black ml-2">
+                  <input type="radio" name="channel" id="smsChannel" />
+                  <label htmlFor="smsChannel" className="text-black ml-2">
                     SMS
                   </label>
                 </div>
               </div>
             </div>
           </div>
-          <div className="flex flex-row gap-2">
+          <div className="flex flex-row gap-2 mt-4">
             <div className="justify-start flex">
               <button className="bg-black text-white rounded-md h-10 p-2">
                 Send Now
@@ -111,7 +112,9 @@ const SendMassCommunication = () => {
               </button>
             </div>
           </div>
-          <Footer />
+          <div>
+            <Footer />
+          </div>
         </div>
       </div>
     </div>
--- tcrp/src/main.jsx
+++ tcrp/src/main.jsx
@@ -6,6 +6,7 @@ import SendMassCommunication from "./components/SendMassCommunication.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <SendMassCommunication />
   </StrictMode>
 );
