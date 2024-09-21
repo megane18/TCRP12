dbbcfd5da1bcd8827d1ee8ef0f207cd67b0eb1e9
Logan Bolton <loganbolton101@gmail.com>
2024-09-21 16:14:39 -0400

    fix main

--- tcrp/src/App.jsx
+++ tcrp/src/App.jsx
@@ -1,37 +1,3 @@
-// import React from 'react';
-// // import EventManager from './components/EventManager';
-// import './App.css';
-// import SignUpForm from './components/SignUpForm';
-// import Login from './components/Login'
-// import Header from './components/Header';
-// import { BrowserRouter as Router } from 'react-router-dom';
-// import React from 'react';
-// import EventManager from './components/EventManager';
-// import './App.css';
-// import SignUpForm from './components/SignUpForm';
-// import Header from './components/Header';
-// import EventDetails from './components/EventDetails';
-// import Footer from './components/Footer';
-
-
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <div className="flex flex-col min-h-screen w-full">
-//         <Header />
-//         <div className="content flex-grow">
-//           <SignUpForm />
-//           <Login />
-//         </div>
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUpForm from './components/SignUpForm';
 import Login from './components/Login'; // Adjust this import as needed
@@ -43,20 +9,6 @@ import Home from './components/Home'
 import './App.css';
 
 const App = () => {
-//   return (
-//     <div className="flex flex-col min-h-screen w-full">
-//       <Header />
-//       <div className="content flex-grow bg-white">
-//         {/* <EventManager /> */}
-//         <EventDetails eventId={1} />
-
-//         {/* <SignUpForm /> */}
-
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// }
 
   return (
     <Router>
--- tcrp/src/main.jsx
+++ tcrp/src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import SendMassCommunication from "./components/SendMassCommunication.jsx";
+// import SendMassCommunication from "./components/SendMassCommunication.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
-    <SendMassCommunication />
+    <App />
+    {/* <SendMassCommunication /> */}
   </StrictMode>
 );
