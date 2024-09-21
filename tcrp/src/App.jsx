7fc0003a7b14110710fa23908054ce9209b97bb4
Oluwatodimu Adegoke <adegokeoluwatodimu@yahoo.com>
2024-09-21 12:54:54 -0500

    send mass communication page

--- tcrp/src/App.jsx
+++ tcrp/src/App.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import EventManager from './components/EventManager';
-import './App.css';
-
+import React from "react";
+import EventManager from "./components/EventManager";
+import SendMassCommunication from "./components/SendMassCommunication";
+import "./App.css";
 
 const App = () => {
   return (
-    <div className ="content">
-      <EventManager />
-    </div>
+    <SendMassCommunication />
+    // <div className="content">
+    //   {/* <EventManager /> */}
+    //   <SendMassCommunication />
+    // </div>
   );
-}
+};
 
 export default App;
