8dd6713c25220ae44c1c825741bf96e3348a219d
Logan Bolton <loganbolton101@gmail.com>
2024-09-22 17:52:04 -0500

    better menu

--- tcrp/src/components/Header.jsx
+++ tcrp/src/components/Header.jsx
@@ -60,11 +60,11 @@ const Header = ({ footerRef }) => {
           </div>
         </Link>
         <div className="relative flex items-center">
-          <nav className="hidden md:flex space-x-6 me-4">
+          <nav className="md:flex space-x-6 me-4">
             <a href="/" className="text-gray-600 hover:text-blue-600">Home</a>
-            <a href="https://www.communityrestorationproject.org/about" className="text-gray-600 hover:text-blue-600">About</a>
+            <a href="https://www.communityrestorationproject.org/about" className="hidden md:flex text-gray-600 hover:text-blue-600">About</a>
             <a href="/dashboard" className="text-gray-600 hover:text-blue-600">Events</a>
-            <a onClick={handleScrollToFooter} className="text-gray-600 hover:text-blue-600">
+            <a onClick={handleScrollToFooter} className="text-gray-600 hover:text-blue-600 hidden md:flex">
               Contact
             </a>
             {/* <Link to="/signup" className="text-gray-600 hover:text-blue-600">Sign Up</Link> */}
@@ -72,7 +72,7 @@ const Header = ({ footerRef }) => {
 
           <button 
             onClick={toggleMenu} 
-            className="MenuButton focus:outline-none bg-gray-100 border-2 border-gray-100 hover:border-gray-200 rounded-md p-2"
+            className="MenuButton focus:outline-none bg-gray-100 border-2 border-gray-100 hover:border-gray-200 rounded-md p-2 mr-2 md:mr-0"
           >
             <Menu className="text-gray-600" size={24} />
           </button>
@@ -80,25 +80,15 @@ const Header = ({ footerRef }) => {
           {isMenuOpen && (
             <nav 
               ref={menuRef} // Attach ref to the menu
-              className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-50 md:hidden"
+              className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-50"
               style={{ top: 'calc(100% + 8px)' }} // Ensures the dropdown appears below the button
             >
               <ul>
                 <li>
-                  <Link to="/" className="block px-4 py-2 text-gray-800 hover:bg-gray-100" onClick={handleMenuItemClick}>Home</Link>
+                  <Link to="/admin" className="block px-4 py-2 text-gray-800 hover:bg-gray-100" onClick={handleMenuItemClick}>Admin</Link>
                 </li>
                 <li>
-                  <a href="https://www.communityrestorationproject.org/about" className="block px-4 py-2 text-gray-800 hover:bg-gray-100" onClick={handleMenuItemClick}>About</a>
-                </li>
-                <li>
-                  <Link to="/dashboard" className="block px-4 py-2 text-gray-800 hover:bg-gray-100" onClick={handleMenuItemClick}>Events</Link>
-                </li>
-                <li>
-                  {/* Use the new handleContactClick to handle both actions */}
-                  <a onClick={handleContactClick} className="block px-4 py-2 text-gray-800 hover:bg-gray-100">Contact</a>
-                </li>
-                <li>
-                  <Link to="/signup" className="block px-4 py-2 text-gray-800 hover:bg-gray-100" onClick={handleMenuItemClick}>Sign Up</Link>
+                  <Link to="/signup" className="block px-4 py-2 text-gray-800 hover:bg-gray-100" onClick={handleMenuItemClick}>Logout</Link>
                 </li>
               </ul>
             </nav>
